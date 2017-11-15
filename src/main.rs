b6d3e24a88375e8fcaa96d3ac76021b5f283e98b
atilla_14 <sathyanarayanravi@gmail.com>
2017-11-15 09:07:21 +0530

    Solve set 1 - challenge 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate rustc_serialize as serialize;
+
+use serialize::base64::{self, ToBase64};
+use serialize::hex::FromHex;
+
 fn main() {
-    println!("Hello, world!");
+    let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    // let result = input.from_hex().unwrap().as_slice().to_base64(base64::STANDARD);
+    let result = hex_to_base64(input);
+    println!("This is the input -> {}", input);
+    println!("This is the result -> {}", result);
+}
+
+fn hex_to_base64(hex_string: &str) -> String {
+    return hex_string.from_hex().unwrap().as_slice().to_base64(base64::STANDARD);
 }
